a6c85c32a77b906ea7d02f044c4b19c1188a7173
Izzet Mustafaiev <izzet@mustafaiev.com>
2018-02-01 19:32:46 +0200

    Add docs

--- handson/ai/io.py
+++ handson/ai/io.py
@@ -5,6 +5,16 @@ IO_CHUNK_SIZE = 1024
 
 
 def get_file_from(url, filename):
+    """Encapsulates capability to download big file from given URL in the efficient way.
+
+    Args:
+        url: The URL to download file from.
+        filename: The name of the file where downloaded content will be saved.
+
+    Returns:
+        The absolute path to downloaded file.
+
+    """
     if not os.path.exists(filename):
         request = req.Request(url)
         request.add_header('User-Agent', 'curl/7.55.1')
